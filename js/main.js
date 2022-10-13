b02860e210cb1eef85800012f0799aa20a493698
dltkdals20 <sangming2007@naver.com>
2022-10-13 21:37:09 +0900

    웹 만들기

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@
 //     $('.gnb').addClass('on')
 // })
 
-document.querySelector('button').addEventListener('click', function () {
-    this.classList.toggle('on')
+document.querySelector('.top_banner button').addEventListener('click', function () {
+    document.querySelector('.top_banner').style.display = 'none'
 })
