f1ef433ef62086f1126e9e787deb6d24ea945744
dltkdals20 <sangming2007@naver.com>
2022-10-13 21:51:35 +0900

    연습파일

--- js/main.js
+++ js/main.js
@@ -4,4 +4,10 @@
 
 document.querySelector('.top_banner button').addEventListener('click', function () {
     document.querySelector('.top_banner').style.display = 'none'
-})
+});
+
+document.querySelector('.mopen').addEventListener('click', function () {
+    this.classList.toggle('on');
+    document.querySelector('nav').toggle('on');
+});
+
